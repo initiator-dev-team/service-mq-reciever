04dae0462d62b8c6cd547e7c21b6d6be68c63002
Mirtov Sergey <mirtovsb@gmail.com>
2019-05-06 17:50:14 +0700

    Убрал вызов системных переменных

--- rabbitMQ/rabbitMQ.go
+++ rabbitMQ/rabbitMQ.go
@@ -3,7 +3,6 @@ package rabbitMQ
 import (
 	"github.com/streadway/amqp"
 	"log"
-	"os"
 )
 
 func failOnError(err error, msg string) {
@@ -14,10 +13,10 @@ func failOnError(err error, msg string) {
 
 type callback func(string)
 
-func StartRabbit(callback callback) {
+func StartRabbit(callback callback, mqUrl, queueName string) {
 	log.Println("RabbitMQ listener started")
 
-	conn, err := amqp.Dial(os.Getenv("RABBIT_URL"))
+	conn, err := amqp.Dial(mqUrl)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -28,12 +27,12 @@ func StartRabbit(callback callback) {
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		os.Getenv("RABBIT_QUEUE_NAME"), // name
-		false, // durable
-		false, // delete when unused
-		false, // exclusive
-		false, // no-wait
-		nil,   // arguments
+		queueName, // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
